25b888d1adecb9a1bc68932faba5c850cfec7a73
bramwax <andy_bramwell@hotmail.com>
2024-02-10 17:42:04 +0100

    more changes

--- fundamentals/abstract_data_structures/stack_queue/stack_queue/src/App.java
+++ fundamentals/abstract_data_structures/stack_queue/stack_queue/src/App.java
@@ -1,14 +1,14 @@
 public class App {
     public static void main(String[] args) throws Exception {
 
-        // Stack nameStack = new Stack(2);
-        // nameStack.push("Andy");
-        // nameStack.push("Nate");
-        // nameStack.push("Päherdin");
+        Stack nameStack = new Stack(2);
+        nameStack.push("Andy");
+        nameStack.push("Nate");
+        nameStack.push("Päherdin");
 
-        // nameStack.pop();
-        // nameStack.pop();
-        // nameStack.pop();
+        nameStack.pop();
+        nameStack.pop();
+        nameStack.pop();
 
         Queue nameQueue = new Queue(3);
         nameQueue.enqueue("Andy");
